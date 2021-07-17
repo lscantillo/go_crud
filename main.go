1f75818d29b5c984e08f50ae068233afde022ed8
lscantillo <luissebastiancantillo@gmail.com>
2021-07-17 14:57:36 -0500

    [WIP]: Initial conection to DB

--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func conexionDB() (conexion *sql.DB) {
+	Driver := "mysql"
+	User := "root"
+	Password := ""
+	Name := "go_crud"
+	conexion, err := sql.Open(Driver, User+":"+Password+"@tcp(127.0.0.1)/"+Name)
+	if err != nil {
+		panic(err.Error())
+	}
+	return conexion
+}
 
 var plantillas = template.Must(template.ParseGlob("templates/*"))
 
 func main() {
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/crear", Create)
 	log.Println("Server running...")
 	http.ListenAndServe(":8080", nil)
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "Hola")
+	establishConexion := conexionDB()
+	insertRecords, err := establishConexion.Prepare("INSERT INTO employess(name,email) VALUES ('Oscar','correo@gmail.com')")
+	if err != nil {
+		panic(err.Error())
+	}
+	insertRecords.Exec()
 	plantillas.ExecuteTemplate(w, "home", nil)
+}
+func Create(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hola")
+	plantillas.ExecuteTemplate(w, "create", nil)
 }
